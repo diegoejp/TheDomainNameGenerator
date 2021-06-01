e4f60b798485fbed7841d651eacbe0796c3ad1c1
diegoejp <d.jacobpulgar@gmail.com>
2021-06-01 14:05:22 -0400

    no funciona js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let myDiv = document.querySelector("#myDiv");
+
+  for (let pro in pronoun) {
+    for (let ad in adj) {
+      for (let no in noun) {
+        //console.log(pronoun[pro]+adj[ad]+noun[no]);
+        let p = document.createElement("P");
+        p.innerHTML = pronoun[pro] + adj[ad] + noun[no] + ".com";
+        myDiv.appendChild(p);
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
