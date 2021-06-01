df2975b22001a520561cec15381c5654d4b16042
diegoejp <d.jacobpulgar@gmail.com>
2021-06-01 14:46:17 -0400

    funciona perfectamente

--- src/app.js
+++ src/app.js
@@ -9,14 +9,19 @@ window.onload = function() {
   let noun = ["jogger", "racoon"];
 
   let myDiv = document.querySelector("#myDiv");
+  let divino = document.querySelector("#div1");
 
   for (let pro in pronoun) {
     for (let ad in adj) {
       for (let no in noun) {
         //console.log(pronoun[pro]+adj[ad]+noun[no]);
         let p = document.createElement("P");
+        let p1 = document.createElement("P");
         p.innerHTML = pronoun[pro] + adj[ad] + noun[no] + ".com";
+        p1.innerHTML = pronoun[pro] + adj[ad] + noun[no] + ".es";
+
         myDiv.appendChild(p);
+        divino.appendChild(p1);
       }
     }
   }
